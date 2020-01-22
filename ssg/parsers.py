9d6ce0b7d07ea53c9f0aef666f65230f86d3005c
Tom Bell <tjbell@gmail.com>
2020-01-22 13:36:15 -0700

    feat: copy

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -6,13 +6,14 @@ from pathlib import Path
 from markdown import markdown
 from docutils.core import publish_parts
 
+
 class Parser:
     extensions = []
 
     def valid_extension(self, extension):
         return extension in self.extensions
 
-    def parse(self, path, dest):
+    def parse(self, path, source, dest):
         raise NotImplementedError
 
     def read(self, path):
@@ -24,29 +25,31 @@ class Parser:
         with open(full_path, 'w') as file:
             file.write(content)
 
-    def copy(self, path, dest):
-        pass
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
 
 
 class MarkdownParser(Parser):
 
     extensions = ['.md']
 
-    def parse(self, path, dest):
+    def parse(self, path, source, dest):
         content = markdown(self.read(path))
         self.write(path, dest, content)
+
 
 class ReStructuredTextParser(Parser):
 
     extensions = ['.rst']
 
-    def parse(self, path, dest):
+    def parse(self, path, source, dest):
         content = publish_parts(self.read(path), writer_name='html5')
         self.write(path, dest, content['html_body'])
+
 
 class ResourceParser(Parser):
 
     extensions = ['.jpg', '.png', '.gif', '.css', '.html']
 
-    def parse(self, path, dest):
-        self.copy(path, dest)
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
--- ssg/site.py
+++ ssg/site.py
@@ -23,7 +23,7 @@ class Site:
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
         if parser is not None:
-            parser.parse(path, self.dest)
+            parser.parse(path, self.source, self.dest)
         else:
             print('Error')
 
