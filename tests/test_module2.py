22312080b96908ba546feb7e28041dc27d294136
Tom Bell <tjbell@gmail.com>
2020-02-27 15:16:09 -0700

    test: module tests t7

--- tests/test_module2.py
+++ tests/test_module2.py
@@ -26,7 +26,6 @@ def test_parser_base_class_module2(parse):
         and extensions.code.value.type == "list"
         and str(extensions.code.annotation) == "List[str]"
     )
-
     assert extensions_exist, (
         "Have you created a variable called `extensions` and assigned it an empty list?"
         " Have you given it a type annotation of `List[str]`?"
@@ -40,9 +39,6 @@ def test_parser_valid_extension_function_module2(parse):
 
     parsers = parse("parsers")
     assert parsers.success, parsers.message
-
-    typing_import = "List" in parsers.get_from_import("typing")
-    assert typing_import, "Have you imported `List` from `typing`?"
 
     parser_class = parsers.get_by_name("class", "Parser")
 
@@ -71,9 +67,9 @@ def test_parser_valid_extension_function_module2(parse):
     assert return_exist, "Do you have a `return` statement?"
 
     in_statement = (
-        return_statement.value.comparison.first.value == "extension"
-        and return_statement.value.comparison.value.first == "in"
-        and str(return_statement.value.comparison.second) == "self.extensions"
+        return_statement.comparison.first.value == "extension"
+        and return_statement.comparison.value.first == "in"
+        and str(return_statement.comparison.second) == "self.extensions"
     )
 
     assert (
@@ -88,9 +84,6 @@ def test_parser_parse_function_module2(parse):
 
     parsers = parse("parsers")
     assert parsers.success, parsers.message
-
-    typing_import = "List" in parsers.get_from_import("typing")
-    assert typing_import, "Have you imported `List` from `typing`?"
 
     parser_class = parsers.get_by_name("class", "Parser")
     assert (
@@ -132,9 +125,6 @@ def test_parser_read_function_module2(parse):
     parsers = parse("parsers")
     assert parsers.success, parsers.message
 
-    typing_import = "List" in parsers.get_from_import("typing")
-    assert typing_import, "Have you imported `List` from `typing`?"
-
     parser_class = parsers.get_by_name("class", "Parser")
     assert (
         parser_class.exists
@@ -173,22 +163,104 @@ def test_parser_read_function_module2(parse):
         and node[2].type == "call",
     ) is not None
 
-    assert return_read_call, "Are you return a call to `read()` on `file`?"
+    assert return_read_call, "Are you returning a call to `read()` on `file`?"
 
 
-"""
 @pytest.mark.test_parser_write_function_module2
 def test_parser_write_function_module2(parse):
     # def write(self, path, dest, content, ext=".html"):
     #     full_path = dest / path.with_suffix(ext).name
-    assert False
+
+    parsers = parse("parsers")
+    assert parsers.success, parsers.message
+
+    parser_class = parsers.get_by_name("class", "Parser")
+    assert (
+        parser_class.exists
+    ), "Have you created a class called `Parser` in the `parsers.py` file?"
+
+    write = parsers.get_by_name("def", "write", parser_class.code)
+    assert write.exists, "Have you created a method called `write` in the `Parser` class?"
+
+    self_arg = parsers.get_by_value("def_argument", "self", write.code)
+    assert self_arg.exists, "Does the `write` method have a `self` argument?"
+
+    path_arg = parsers.get_by_value("def_argument", "path", write.code)
+    assert path_arg.exists, "Does the `write` method have a `path` argument?"
+
+    dest_arg = parsers.get_by_value("def_argument", "dest", write.code)
+    assert dest_arg.exists, "Does the `write` method have a `dest` argument?"
+
+    content_arg = parsers.get_by_value("def_argument", "content", write.code)
+    assert content_arg.exists, "Does the `write` method have a `content` argument?"
+
+    ext_arg = parsers.get_by_value("def_argument", "ext", write.code)
+    ext_arg_exists = ext_arg.exists and ext_arg.code.value.value.replace("'", '"') == '".html"'
+    assert ext_arg_exists, "Does the `write` method have a `ext` argument set to the correct default argument?"
+
+    assignment = write.code.assignment
+    full_path_exists = assignment.target.value == "full_path"
+    assert full_path_exists, "Do you have a variable called `full_path` that is set correctly?"
+
+    right_side = assignment.binary_operator.find(
+        "atomtrailers",
+        lambda node: node[0].value == "path"
+        and node[1].value == "with_suffix"
+        and node[2].type == "call"
+        and node[2].call_argument.name.value == "ext"
+        and node[3].value == "name",
+    ) is not None
+
+    correct_path = (
+        assignment.binary_operator.value == "/"
+        and assignment.binary_operator.first.value == "dest"
+        and right_side
+    )
+    assert correct_path, "Do you have a variable called `full_path` that is set to `dest / path.with_suffix(ext).name`?"
 
 
 @pytest.mark.test_parser_write_function_open_module2
 def test_parser_write_function_open_module2(parse):
     # with open(full_path, "w") as file:
     #     file.write(content)
-    assert False
+
+    parsers = parse("parsers")
+    assert parsers.success, parsers.message
+
+    parser_class = parsers.get_by_name("class", "Parser")
+    assert (
+        parser_class.exists
+    ), "Have you created a class called `Parser` in the `parsers.py` file?"
+
+    write = parsers.get_by_name("def", "write", parser_class.code)
+    assert write.exists, "Have you created a method called `write` in the `Parser` class?"
+
+    with_exists = write.code.with_ is not None
+    assert with_exists, "Do you have a `with` statement in the `write` method?"
+
+    open_call = parsers.get_call("open", write.code.with_)
+    assert open_call.exists, "Do you have a call to `open` in your `with` statement?"
+
+    open_args = parsers.get_args(open_call.code)
+    
+    args_exist = (
+        "None:full_path" in open_args
+        and 'None:"w"' in open_args
+    )
+    assert args_exist, "Are you passing `open` the correct arguments?"
+
+    with_as = write.code.with_context_item.as_.value == "file"
+    assert with_as, "Does your `with ` have and `as file` statement?"
+
+    write_call = write.code.with_.find(
+        "atomtrailers",
+        lambda node: node[0].value == "file"
+        and node[1].value == "write"
+        and node[2].type == "call"
+        and node[2].name.value == "content",
+    ) is not None
+
+    assert write_call, "Are you calling `write()` on `file` and passing in `content`?"
 
 
 @pytest.mark.test_parser_copy_function_module2
@@ -196,7 +268,58 @@ def test_parser_copy_function_module2(parse):
     # import shutil
     # def copy(self, path, source, dest):
     #     shutil.copy2(path, dest / path.relative_to(source))
-    assert False
+
+
+    parsers = parse("parsers")
+    assert parsers.success, parsers.message
+
+    parser_class = parsers.get_by_name("class", "Parser")
+    assert (
+        parser_class.exists
+    ), "Have you created a class called `Parser` in the `parsers.py` file?"
+
+    copy = parsers.get_by_name("def", "copy", parser_class.code)
+    assert copy.exists, "Have you created a method called `copy` in the `Parser` class?"
+
+    self_arg = parsers.get_by_value("def_argument", "self", copy.code)
+    assert self_arg.exists, "Does the `copy` method have a `self` argument?"
+
+    path_arg = parsers.get_by_value("def_argument", "path", copy.code)
+    assert path_arg.exists, "Does the `copy` method have a `path` argument?"
+
+    source_arg = parsers.get_by_value("def_argument", "source", copy.code)
+    assert source_arg.exists, "Does the `copy` method have a `source` argument?"
+
+    dest_arg = parsers.get_by_value("def_argument", "dest", copy.code)
+    assert dest_arg.exists, "Does the `copy` method have a `dest` argument?"
+
+    copy2_call = copy.code.find(
+        "atomtrailers",
+        lambda node: node[0].value == "shutil"
+        and node[1].value == "copy2"
+        and node[2].type == "call",
+    )
+    copy2_call_exist = copy2_call is not None
+    assert copy2_call_exist, "Are you calling `shutil.copy2()` in the `copy` method?"
+
+    first_arg = copy2_call.call_argument.name.value == "path"
+    assert first_arg, "Are you passing `path` as the first argument to `shutil.copy2()`?"
+
+    right_side = copy2_call.binary_operator.find(
+        "atomtrailers",
+        lambda node: node[0].value == "path"
+        and node[1].value == "relative_to"
+        and node[2].type == "call"
+        and node[2].call_argument.name.value == "source",
+    ) is not None
+
+    correct_path = (
+        copy2_call.binary_operator.value == "/"
+        and copy2_call.binary_operator.first.value == "dest"
+        and right_side
+    )
+
+    assert correct_path, "Are you passing `dest / path.relative_to(source)` as the first argument to `shutil.copy2()`?"
 
 
 @pytest.mark.test_parser_resource_class_module2
@@ -206,9 +329,33 @@ def test_parser_resource_class_module2(parse):
 
     #     def parse(self, path, source, dest):
     #         self.copy(path, source, dest)
+
+    parsers = parse("parsers")
+    assert parsers.success, parsers.message
+
+    resource_parser_class = parsers.get_by_name("class", "ResourceParser")
+    assert (
+        resource_parser_class.exists
+    ), "Have you created a class called `ResourceParser` in the `parsers.py` file?"
+
+    inheriting = resource_parser_class.code.inherit_from.name.value == "Parser"
+    assert inheriting, "Is `ResourceParser` a sub-class of the `Parser` class?"
+
+    parse = parsers.get_by_name("def", "parse", resource_parser_class.code)
+    assert parse.exists, "Have you created a method called `parse` in the `ResourceParser` class?"
+
+    extensions = parsers.get_by_value("assignment", "extensions", resource_parser_class.code)
+    assert ( 
+        extensions.exists
+    ), "Have you created a variable called `extensions` and assigned it a list of extensions?"
+
+    # TODO: Check for the right extensions
+
+    # TODO: check is copy is being called in the parse method
+
     assert False
 
-
+"""
 @pytest.mark.test_site_parsers_module2
 def test_site_parsers_module2(parse):
     # , parsers=None
